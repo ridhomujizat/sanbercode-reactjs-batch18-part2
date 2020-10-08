fb2b14909c82c8b166698959ae7810f5032ded9c
ridhomujizat <ridhomujizat@gmail.com>
2020-10-08 18:12:51 +0700

    Tugas-9

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{ textAlign: "center" }}>Form Pembelian Buah</h1>
+      <div style={{ "padding-left": "25px" }}>
+        <div style={{ display: "block", "margin-bottom": "1em" }}>
+          <div
+            style={{
+              display: "inline-block",
+              width: "150px",
+              "font-weight": "bold",
+              "font-size": "16px",
+            }}
+          >
+            Nama Pelanggan
+          </div>
+          <input style={{ display: "inline-block" }} />
+        </div>
+        <div style={{ display: "block", "margin-bottom": "1em" }}>
+          <div
+            style={{
+              display: "inline-block",
+              width: "150px",
+              "font-weight": "bold",
+              "font-size": "16px",
+            }}
+          >
+            Daftar Item
+          </div>
+          <div style={{ display: "inline-block" }}>
+            <input type="checkbox" />
+            <label for="Semangka">Semangka</label>
+            <br />
+            <input type="checkbox" />
+            <label for="Jeruk">Jeruk</label>
+            <br />
+            <input type="checkbox" />
+            <label for="Nanas">Nanas</label>
+            <br />
+            <input type="checkbox" />
+            <label for="Salak">Salak</label>
+            <br />
+            <input type="checkbox" />
+            <label for="Angur">Angur</label>
+            <br />
+            <br />
+          </div>
+        </div>
+        <button style={{ padding: "5px", "border-radius": "10px" }}>
+          Kirim
+        </button>
+      </div>
     </div>
   );
 }
